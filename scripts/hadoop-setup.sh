6d3d3739c518fdae37c6116aad55493fe3cba73e
Nikolay <nasilaev@edu.hse.ru>
2024-12-13 00:17:02 +0000

    add script1

--- scripts/hadoop-setup.sh
+++ scripts/hadoop-setup.sh
@@ -57,31 +57,54 @@ validate_and_parse_config() {
     
     # Second line is jump node
     JUMP_LINE=$(sed -n '2p' nodes.txt)
-    JUMP_NODE_IP=$(echo "$JUMP_LINE" | cut -d' ' -f1)
-    JUMP_NODE=$(echo "$JUMP_LINE" | cut -d' ' -f2)
+    JUMP_NODE_IP=$(echo "$JUMP_LINE" | awk '{print $1}')
+    JUMP_NODE=$(echo "$JUMP_LINE" | awk '{print $2}')
     NODES[$JUMP_NODE]=$JUMP_NODE_IP
     info "Jump node: $JUMP_NODE ($JUMP_NODE_IP)"
     
     # Third line is name node
     NAME_LINE=$(sed -n '3p' nodes.txt)
-    NAME_NODE_IP=$(echo "$NAME_LINE" | cut -d' ' -f1)
-    NAME_NODE=$(echo "$NAME_LINE" | cut -d' ' -f2)
+    NAME_NODE_IP=$(echo "$NAME_LINE" | awk '{print $1}')
+    NAME_NODE=$(echo "$NAME_LINE" | awk '{print $2}')
     NODES[$NAME_NODE]=$NAME_NODE_IP
     info "Name node: $NAME_NODE ($NAME_NODE_IP)"
     
     # Fourth line is data node 0
     DN0_LINE=$(sed -n '4p' nodes.txt)
-    DN0_IP=$(echo "$DN0_LINE" | cut -d' ' -f1)
-    DATA_NODE_0=$(echo "$DN0_LINE" | cut -d' ' -f2)
-    NODES[$DATA_NODE_0]=$DN0_IP
-    info "Data node 0: $DATA_NODE_0 ($DN0_IP)"
+    DATA_NODE_0_IP=$(echo "$DN0_LINE" | awk '{print $1}')
+    DATA_NODE_0=$(echo "$DN0_LINE" | awk '{print $2}')
+    NODES[$DATA_NODE_0]=$DATA_NODE_0_IP
+    info "Data node 0: $DATA_NODE_0 ($DATA_NODE_0_IP)"
     
     # Fifth line is data node 1
     DN1_LINE=$(sed -n '5p' nodes.txt)
-    DN1_IP=$(echo "$DN1_LINE" | cut -d' ' -f1)
-    DATA_NODE_1=$(echo "$DN1_LINE" | cut -d' ' -f2)
-    NODES[$DATA_NODE_1]=$DN1_IP
-    info "Data node 1: $DATA_NODE_1 ($DN1_IP)"
+    DATA_NODE_1_IP=$(echo "$DN1_LINE" | awk '{print $1}')
+    DATA_NODE_1=$(echo "$DN1_LINE" | awk '{print $2}')
+    NODES[$DATA_NODE_1]=$DATA_NODE_1_IP
+    info "Data node 1: $DATA_NODE_1 ($DATA_NODE_1_IP)"
+}
+
+test_connectivity() {
+    log "Testing connectivity to all nodes..."
+    
+    for node in "${!NODES[@]}"; do
+        local ip="${NODES[$node]}"
+        info "Testing connection to $node ($ip)"
+        
+        if ! ping -c 1 "$ip" > /dev/null 2>&1; then
+            error "Cannot ping $node ($ip)"
+            continue
+        fi
+        
+        if ! timeout 5 ssh -o BatchMode=yes -o StrictHostKeyChecking=no "team@$ip" "echo 'Connected'" > /dev/null 2>&1; then
+            error "Cannot SSH to $node ($ip). Please check SSH access and password."
+            continue
+        fi
+        
+        info "Successfully connected to $node"
+    done
+    
+    log "All nodes are reachable"
 }
 
 test_connectivity() {
